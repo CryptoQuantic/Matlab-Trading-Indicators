af088364eeb21daea4ede8f655135035a8e41182
Crypto Quantic <johnwilson@Johns-MBP-2.local>
2019-03-30 17:03:08 -0400

    Updated Several Indicators
    Updated sma, ema and tr to similar input format

--- ema.m
+++ ema.m
@@ -1,5 +1,17 @@
-function [vout,status] = ema(data,lag,varargin)
-observ = length(data)
+function [ema_out,status] = ema(data,lag,varargin)
+
+[l,w] = size(data);
+%if row vector, convert to column
+if(w==1) %is column vector
+    nsamples = l;
+else %is row vector
+    data = data'
+    nsamples = w;
+end
+
+
+% Specify k, o input = standard ema, 1 = Wilders Moving Average
+
 if isempty(varargin)
     k = 2/(lag+1);
 else
@@ -9,26 +21,24 @@ end
 
 
 % Preallocate output
-vout   = nan(observ,1);
+ema_out   = nan(nsamples,1);
 % Set status
 status = 1;
-% If the lag is greater than or equal to the number of observations
-if lag >= observ
+% If the lag is greater than or equal to the number of nsample, then bail
+if lag >= nsamples
     status = 0;
     return
 end
-% Calculate the exponential percentage
 
 % Calculate the simple moving average for the first 'exp mov avg' value.
-vout(lag) = sum(data(1:lag))/lag;
+ema_out(lag) = sum(data(1:lag))/lag;
 % K*vin; 1-k
-kvin = data(lag:observ)*k;
-oneK = 1-k;
+kvin = data(lag:nsamples)*k;
 % First period calculation
-vout(lag) = kvin(1)+(vout(lag)*oneK);
+ema_out(lag) = kvin(1)+(ema_out(lag)*(1-k));
 % Remaining periods calculation
-for i1 = lag+1:observ
-    vout(i1) = kvin(i1-lag+1)+(vout(i1-1)*oneK);
+for i1 = lag+1:nsamples
+    ema_out(i1) = kvin(i1-lag+1)+(ema_out(i1-1)*(1-k));
 end
 end
 
--- sma.m
+++ sma.m
@@ -1,12 +1,27 @@
-function sma = sma(data,smoothlength)
-n = length(data);
-sma = zeros(n,1);
-for j = 1:n
-    if j<smoothlength
-        sma(j) = nan;
-    else
-        dataseg = data((j-smoothlength+1):j);
-        sma(j) = sum(dataseg)/smoothlength;
-    end
+function [sma_out,status] = sma(data,lag,varargin)
+
+[l,w] = size(data);
+%if row vector, convert to column
+if(w==1) %is column vector
+    nsamples = l;
+else %is row vector
+    data = data'
+    nsamples = w;
+end
+
+% Preallocate output
+sma_out   = nan(nsamples,1);
+% Set status
+status = 1;
+% If the lag is greater than or equal to the number of nsamples, then bail
+if lag >= nsamples
+    status = 0;
+    return
+end
+
+ma = filter(ones(1,lag)/lag,1,data);
+
+% Fill in the nan's
+sma_out(lag:end) = ma(lag:end);
 end
 
