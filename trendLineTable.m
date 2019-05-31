33e0cc04ae5e02ab3c98c34237631e2df3e50c0b
Crypto Quantic <jwwilson11733@gmail.com>
2019-05-31 14:40:47 -0400

    TrendTable Added

--- trendLineTable.m
+++ trendLineTable.m
@@ -1,1 +1,10 @@
 function trendlineTable = trendLineTable(dema_table)
+%Table Rows: segLows,segHighs,startIndicies,endIndicies,segSlopes,startVals,endVals
+ segLows,segHighs,startIndicies,endIndicies,segSlopes,startVals,endVals
+ dataTableSize = size(dema_table);
+ N = dataTableSize(1);
+ 
+ for j = 1:N
+     
+ end
+end
