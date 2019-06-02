3f276b2fe2c49a9b9d77003a8c542c0ae6636980
Crypto Quantic <jwwilson11733@gmail.com>
2019-06-02 10:52:47 -0400

    Create linearchannel.m

---
+++ linearchannel.m
@@ -0,0 +1,10 @@
+function m,b,sigma = linearchannel(data,lag)
+%Table Rows: segLows,segHighs,startIndicies,endIndicies,segSlopes,startVals,endVals
+ segLows,segHighs,startIndicies,endIndicies,segSlopes,startVals,endVals
+ dataTableSize = size(dema_table);
+ N = dataTableSize(1);
+ 
+ for j = 1:N
+     
+ end
+end
