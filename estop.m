1ec5d03fd0029879852be15eff536dd66c342f24
Crypto Quantic <jwwilson11733@gmail.com>
2019-06-05 10:23:38 -0400

    Update estop.m

--- estop.m
+++ estop.m
@@ -1,4 +1,4 @@
 function estop()
-orderId = IBMatlab('action','sell', 'quantity',1,'Exchange','GLOBEX','symbol','mes','secType','FUT','localsymbol','mesm9','type','MKT');
+orderId = IBMatlab('action','close','Exchange','GLOBEX','symbol','mes','secType','FUT','localsymbol','mesm9','type','MKT');
 pause(1);
 data = IBMatlab('action','CANCEL');
